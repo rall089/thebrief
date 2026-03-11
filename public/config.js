7614f3db897dee6d86e562b457521fbc6e7ed3d9
rall089 <ronnielallman@gmail.com>
2026-03-11 15:20:05 -0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -3,5 +3,5 @@
 // Paste your two Supabase values below
 // ─────────────────────────────────────────────
 
-window.SUPABASE_URL = "YOUR_SUPABASE_URL_HERE";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY_HERE";
+window.SUPABASE_URL = "https://qvjmnjbqkstazzmcoodd.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2am1uamJxa3N0YXp6bWNvb2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1ODEzNDMsImV4cCI6MjA4ODE1NzM0M30.wFKn06dsgkLFRXTIJCMsu3Q_9_N54Yhb23oLn6XU51o";
